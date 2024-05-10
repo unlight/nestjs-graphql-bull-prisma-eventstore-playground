f315f6adba4d2f1d0ec7984239d5dd047d609a29
Roman <s91630277@gmail.com>
2024-05-10 16:32:22 +0400

    schema

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,18 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import sonarjs from 'eslint-plugin-sonarjs';
 import prettier from 'eslint-plugin-prettier/recommended';
+// @ts-expect-error missing types
 import * as unicorn from 'eslint-plugin-unicorn';
+// @ts-expect-error missing types
 import perfectionist from 'eslint-plugin-perfectionist';
 import 'eslint-plugin-only-warn';
 
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
   {
     ignores: ['dist/', 'coverage/', 'eslint.tsconfig.mjs'],
-  },
-  {
     languageOptions: {
+      globals: globals.browser,
       parserOptions: {
         project: ['./tsconfig.json'],
         warnOnUnsupportedTypeScriptVersion: false,
@@ -20,14 +22,13 @@ export default [
       },
     },
   },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   sonarjs.configs.recommended,
-  unicorn.configs['flat/recommended'],
   prettier,
   {
+    ...unicorn.configs['flat/recommended'],
     rules: {
       'unicorn/prevent-abbreviations': [
         'warn',
@@ -51,6 +52,18 @@ export default [
           order: 'asc',
         },
       ],
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    rules: {
+      'consistent-return': 0,
+      'max-lines': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-floating-promises': 0,
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/camelcase': 0,
+      'import/max-dependencies': 0,
     },
   },
 ];
